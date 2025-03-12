63761f37fceb71e0b8dd4a8e481eaf74ce813704
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-12 21:15:38 +0900

    tow sum

---
+++ Algorithm/go/blind75/1-TwoSum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/*
+search one pair that they add up to target.
+*/
+func twoSum(nums []int, target int) []int {
+	cache := map[int]int{}
+
+	for i, n := range nums {
+		diff := target - n
+		if cacheIndex, ok := cache[diff]; ok {
+			return []int{cacheIndex, i}
+		}
+		cache[n] = i
+	}
+
+	return nil //cannot find pair
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
