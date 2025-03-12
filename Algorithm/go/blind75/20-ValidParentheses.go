c05cbb45775f57feb033bff48dcdf21e2c41ea9e
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-13 07:55:01 +0900

    isValidParentheses

---
+++ Algorithm/go/blind75/20-ValidParentheses.go
@@ -0,0 +1,25 @@
+package main
+
+func isValidParentheses(s string) bool {
+	stack := []rune{}
+
+	for _, r := range s {
+		if r == '(' || r == '{' || r == '[' {
+			stack = append(stack, r)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			if (r == ')' && top != '(') ||
+				(r == '}' && top != '{') ||
+				(r == ']' && top != '[') {
+				return false
+			}
+
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
