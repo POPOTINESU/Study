14794ee54125e75d5aa8f472371d2ad5b318b06b
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-15 14:37:05 +0900

    3-LongestSubstringWithoutRepeatingCharacters

--- Algorithm/python/Blind75/3-LongestSubstringWithoutRepeatingCharacters.py
+++ Algorithm/python/Blind75/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,24 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if s == "":
-            return 0
+        cache = {}
+        longest = 0
 
-        cache = dict()
-        longest = 1
-        s_length = len(s)
-        for i in range(s_length):
-            right = i + 1
-            count = 1
-            while right < s_length:
-                hit = cache.get(s[right])
-                if hit and hit >= 1:
-                    longest = max(longest, count)
-                    break
+        # Use sliding window algorithm
+        left = 0
+        for right in range(len(s)):
+            if s[right] in cache and cache[s[right]] >= left:
+                left = cache[s[right]] + 1
 
-                count += 1
-                right += 1
+            cache[s[right]] = right
+            longest = max(longest, right - left + 1)
 
         return longest
+
 
 if __name__ == "__main__":
     print(Solution().lengthOfLongestSubstring("abcabcbb"))
