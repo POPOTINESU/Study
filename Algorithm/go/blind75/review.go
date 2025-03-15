70c5b781667a97cd9f0dbe8ac3ec0896e77c675e
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-15 12:03:42 +0900

    review: 104-MaximumDepthOfBinaryTree

--- Algorithm/go/blind75/review.go
+++ Algorithm/go/blind75/review.go
@@ -1,16 +1,17 @@
 package main
 
-func review(root *TreeNode) *TreeNode {
+func review(root *TreeNode) int {
 	if root == nil {
-		return nil
+		return 0
 	}
 
-	left := review(root.Left)
-	right := review(root.Right)
+	leftDepth := review(root.Left)
+	rightDepth := review(root.Right)
 
-	// invert tree
-	root.Left = right
-	root.Right = left
-
-	return root
+	// Add 1 to include current node in the depth count
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
 }
