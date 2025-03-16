aabb48d89de263f82ea56c00c153c7eda28e8332
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-16 14:48:20 +0900

    review: clone graph

--- Algorithm/python/Blind75/review.py
+++ Algorithm/python/Blind75/review.py
@@ -1,18 +1,48 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        s_length = len(s)
-        if s_length < 2:
-            return s_length
+from collections import deque
+from typing import Optional
 
-        left = 0
-        longest = 1
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return None
+
         visited = {}
 
-        for right in range(s_length):
-            if s[right] in visited and visited[s[right]] >= left:
-                left = visited[s[right]] + 1
+        def dfs(node: Node) -> Node:
+            if node in visited:
+                return visited[node]
 
-            visited[s[right]] = right
-            longest = max(longest, right - left + 1)
+            clone_node = Node(node.val)
+            visited[node] = clone_node
+            for neighbor in node.neighbors:
+                clone_node.neighbors.append(dfs(neighbor))
 
-        return longest
+            return clone_node
+
+        return dfs(node)
+
+    def bfsCloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return None
+
+        visited: dict[Node, Node] = {}
+        # queue include original nodes
+        queue = deque([node])
+        visited[node] = Node(node.val)
+
+        while queue:
+            curr = queue.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in visited and isinstance(neighbor, Node):
+                    visited[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                visited[curr].neighbors.append(visited[neighbor])
+
+        return visited[node]
