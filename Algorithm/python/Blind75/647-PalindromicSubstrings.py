315364beae908b95399f72aba321e5004aece113
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-17 19:47:02 +0900

    647-PalindromicSubstrings

---
+++ Algorithm/python/Blind75/647-PalindromicSubstrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        size = len(s)
+        result = 0
+
+        def count_palindromes(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < size and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+
+            return count
+
+        for i in range(size):
+            result += count_palindromes(i, i)
+            result += count_palindromes(i, i + 1)
+
+        return result
