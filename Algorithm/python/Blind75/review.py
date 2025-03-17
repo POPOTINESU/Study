a0b8b69e71313fd429457e6ea2d41c4ea4b9d0ec
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-18 07:33:21 +0900

    review: 647-PalindromicSubstrings

--- Algorithm/python/Blind75/review.py
+++ Algorithm/python/Blind75/review.py
@@ -1,32 +1,19 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isSameTree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-            if not root and not subRoot:
-                return True
-            if not root or not subRoot or root.val != subRoot.val:
-                return False
+    def countSubstrings(self, s: str) -> int:
+        if len(s) < 1:
+            return len(s)
 
-            return isSameTree(root.left, subRoot.left) and isSameTree(
-                root.right, subRoot.right
-            )
+        def count_palindromic_substrings(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
 
-        if not subRoot:
-            return True
-
-        if not root:
-            return False
-
-        if root.val == subRoot.val and isSameTree(root, subRoot):
-            return True
-
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        result = 0
+        for i in range(len(s)):
+            result += count_palindromic_substrings(i, i)
+            result += count_palindromic_substrings(i, i + 1)
+        
+        return result
