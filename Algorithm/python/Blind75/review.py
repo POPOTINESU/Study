847bd761df5696c93e5eae3932e283f05c9edcbc
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-18 08:13:41 +0900

    review: 139-WordBreak

--- Algorithm/python/Blind75/review.py
+++ Algorithm/python/Blind75/review.py
@@ -2,17 +2,15 @@ from typing import List
 
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-
-        max_area = 0
-        while left < right:
-            low_h = min(height[left], height[right])
-            max_area = max(max_area, low_h * (right - left))
-
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-
-        return max_area
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for right in range(1, len(s) + 1):
+            for left in range(right):
+                if dp[left] and s[left:right] in word_set:
+                    dp[right] = True
+                    break
+        
+        return dp[len(s)]
