6f461ed489775ca6404a077d092e9a733f8a15e3
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-23 12:29:40 +0900

    review: 70. Climbing Stairs

--- Algorithm/python/Blind75/review.py
+++ Algorithm/python/Blind75/review.py
@@ -1,20 +1,9 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
-        size = len(s)
-        if size < 2:
-            return size
-        result = 0
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
 
-        def count_palindromes(left: int, right: int) -> int:
-            count = 0
-            while left >= 0 and right < size and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
-            return count
-
-        for i in range(size):
-            result += count_palindromes(i, i) + count_palindromes(i, i + 1)
-
-        return result
+        return dp[n]
