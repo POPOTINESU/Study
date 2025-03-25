6a37bde5a97e210ce74f4ab5cf5e80b609e424de
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-03-26 08:41:09 +0900

    review: 271-EncodeAndDecodeStrings

--- Algorithm/python/Blind75/review.py
+++ Algorithm/python/Blind75/review.py
@@ -1,30 +1,22 @@
-from typing import Optional
+from typing import List
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        return "".join(f"{len(s)}:{s}" for s in strs)
 
+    def decode(self, s: str) -> List[str]:
+        res = []
 
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root or not subRoot:
-            return False
+        start = 0
+        while start < len(s):
+            colon = start
+            while s[colon] != ":":
+                colon += 1
+            length = int(s[start:colon])
+            colon += 1
+            end = colon + length
+            res.append(s[colon:end])
+            start = end
 
-        if root.val == subRoot.val and self.isSameTree(root, subRoot):
-            return True
-
-        return self.isSubtree(root.left, subRoot) | self.isSubtree(root.right, subRoot)
-
-    def isSameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot:
-            return True
-
-        if not root or not subRoot or root.val != subRoot.val:
-            return False
-
-        return self.isSameTree(root.left, subRoot.left) and self.isSameTree(
-            root.right, subRoot.right
-        )
+        return res
